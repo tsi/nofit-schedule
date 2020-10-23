270e19be9889e7fe0b4787139d6eaacd28f71342
Tsachi Shlidor <tsi.shlidor@gmail.com>
2020-10-23 11:54:23 +0300

    Updates

---
+++ precache-manifest.b79b776d6d68a18baca178954f6a9624.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dcff0c7ef8df29f2ed11480449c8d09",
+    "url": "/nofit-schedule/index.html"
+  },
+  {
+    "revision": "951e2d2d3980c8bc11ac",
+    "url": "/nofit-schedule/static/css/main.9ca5ea04.chunk.css"
+  },
+  {
+    "revision": "e24b9600d42984590874",
+    "url": "/nofit-schedule/static/js/2.b413901a.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/nofit-schedule/static/js/2.b413901a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951e2d2d3980c8bc11ac",
+    "url": "/nofit-schedule/static/js/main.63cae1b2.chunk.js"
+  },
+  {
+    "revision": "d26042d424f13574328b",
+    "url": "/nofit-schedule/static/js/runtime-main.d8dbc70a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nofit-schedule/static/media/logo.5d5d9eef.svg"
+  }
+]);
