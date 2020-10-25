9f5c751a0367f3fce585417d1c5782233494cd9c
Tsachi Shlidor <tsi.shlidor@gmail.com>
2020-10-25 16:13:58 +0200

    localstorage

--- src/App.js
+++ src/App.js
@@ -1,84 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import config from './config';
 import logo from './logo.svg';
 import Links from './Links';
+import { initGapiClient } from './gapiUtils';
 import './App.css';
-
-const initGapiClient = (setData) => {
-  // 2. Initialize the JavaScript client library.
-  window.gapi.client
-    .init({
-      apiKey: config.apiKey,
-      discoveryDocs: config.discoveryDocs,
-    })
-    .then(() => {
-      // 3. Initialize and make the API request.
-      getData(setData);
-    });
-};
-
-const getData = (setData) => {
-  window.gapi.client.load("sheets", "v4", () => {
-    window.gapi.client.sheets.spreadsheets.values
-      .batchGet({
-        spreadsheetId: config.spreadsheetId,
-        ranges: ["'מערכת'!B2:H", "'תכנים'!A1:H"]
-      })
-      .then(
-        response => {
-          const data = parseData(response.result.valueRanges);
-          setData(data);
-        },
-        response => {
-          console.error(response.result.error);
-        }
-      );
-  });
-}
-
-const parseData = (data) => {
-  console.log(data);
-  const rowList = data[0].values;
-  const days = data[0].values[0];
-  const daysArr = [];
-  const hoursList = [];
-  const dayList = [];
-  const lessons = {};
-  for (let dayIdx = 1; dayIdx < days.length; dayIdx++) {
-    const day = days[dayIdx];
-    dayList.push(day);
-    for (let rowIdx = 1; rowIdx < rowList.length; rowIdx++) {
-      const time = rowList[rowIdx][0];
-      const name = rowList[rowIdx][dayIdx];
-      if (name && !lessons[name]) {
-        const lesson = data[1].values.find(l => l[0] === name);
-        if (lesson) {
-          lessons[name] = {
-            label: name,
-            who: lesson.slice(1, 5).map((val, i) => val === 'TRUE' ? i + 1 : false),
-            link: lesson[5],
-            tooltip: lesson[6],
-            color: lesson[7]
-          }
-        }
-      }
-      if (hoursList.indexOf(time) === -1) hoursList.push(time);
-      daysArr[day] = daysArr[day] ? daysArr[day] : [];
-      daysArr[day][time] = daysArr[day][time] ? daysArr[day][time] : [];
-      daysArr[day][time].push(name);
-    }
-  }
-  return {
-    days: dayList,
-    hours: hoursList,
-    lessons: lessons,
-    schedule: daysArr
-  };
-}
 
 function App() {
   const [data, setData] = useState(null);
-  const [who, setWho] = useState(0);
+  const [who, setWho] = useState(localStorage.getItem('who') || 0);
 
   useEffect(() => {
     window.gapi.load("client", () => initGapiClient(setData))
@@ -86,6 +14,7 @@ function App() {
 
   const handleChange = (e) => {
     setWho(e.target.value);
+    localStorage.setItem('who', e.target.value);
   }
 
   console.log(data);
---
+++ src/gapiUtils.js
@@ -0,0 +1,73 @@
+import config from './config';
+
+export const initGapiClient = (setData) => {
+  // Initialize the JavaScript client library.
+  window.gapi.client
+    .init({
+      apiKey: config.apiKey,
+      discoveryDocs: config.discoveryDocs,
+    })
+    .then(() => {
+      // Initialize and make the API request.
+      getData(setData);
+    });
+};
+
+export const getData = (setData) => {
+  window.gapi.client.load("sheets", "v4", () => {
+    window.gapi.client.sheets.spreadsheets.values
+      .batchGet({
+        spreadsheetId: config.spreadsheetId,
+        ranges: ["'מערכת'!B2:H", "'תכנים'!A1:H"]
+      })
+      .then(
+        response => {
+          const data = parseData(response.result.valueRanges);
+          setData(data);
+        },
+        response => {
+          console.error(response.result.error);
+        }
+      );
+  });
+}
+
+export const parseData = (data) => {
+  console.log(data);
+  const rowList = data[0].values;
+  const days = data[0].values[0];
+  const daysArr = [];
+  const hoursList = [];
+  const dayList = [];
+  const lessons = {};
+  for (let dayIdx = 1; dayIdx < days.length; dayIdx++) {
+    const day = days[dayIdx];
+    dayList.push(day);
+    for (let rowIdx = 1; rowIdx < rowList.length; rowIdx++) {
+      const time = rowList[rowIdx][0];
+      const name = rowList[rowIdx][dayIdx];
+      if (name && !lessons[name]) {
+        const lesson = data[1].values.find(l => l[0] === name);
+        if (lesson) {
+          lessons[name] = {
+            label: name,
+            who: lesson.slice(1, 5).map((val, i) => val === 'TRUE' ? i + 1 : false),
+            link: lesson[5],
+            tooltip: lesson[6],
+            color: lesson[7]
+          }
+        }
+      }
+      if (hoursList.indexOf(time) === -1) hoursList.push(time);
+      daysArr[day] = daysArr[day] ? daysArr[day] : [];
+      daysArr[day][time] = daysArr[day][time] ? daysArr[day][time] : [];
+      daysArr[day][time].push(name);
+    }
+  }
+  return {
+    days: dayList,
+    hours: hoursList,
+    lessons: lessons,
+    schedule: daysArr
+  };
+}
